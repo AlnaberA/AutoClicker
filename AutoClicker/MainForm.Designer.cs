d2362f7f8b0efc200654e9afa8add3ea18f7a714
Alaa Al Naber <alnaberalaa@gmail.com>
2018-04-25 16:34:58 -0400

    finish ui for click interval

--- AutoClicker/MainForm.Designer.cs
+++ AutoClicker/MainForm.Designer.cs
@@ -29,40 +29,40 @@
         private void InitializeComponent()
         {
             this.clickInterval_groupBox = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.hours_label = new System.Windows.Forms.Label();
             this.hours_textBox = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.minutes_label = new System.Windows.Forms.Label();
+            this.minutes_textBox = new System.Windows.Forms.TextBox();
+            this.seconds_label = new System.Windows.Forms.Label();
+            this.seconds_textBox = new System.Windows.Forms.TextBox();
+            this.milliseconds_label = new System.Windows.Forms.Label();
+            this.milliseconds_textBox = new System.Windows.Forms.TextBox();
+            this.range_label = new System.Windows.Forms.Label();
+            this.start_textBox = new System.Windows.Forms.TextBox();
+            this.end_textBox = new System.Windows.Forms.TextBox();
+            this.start_label = new System.Windows.Forms.Label();
+            this.end_label = new System.Windows.Forms.Label();
+            this.setTimeDefault_radioButton = new System.Windows.Forms.RadioButton();
+            this.setTimeRand_radioButton = new System.Windows.Forms.RadioButton();
             this.clickInterval_groupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // clickInterval_groupBox
             // 
-            this.clickInterval_groupBox.Controls.Add(this.radioButton2);
-            this.clickInterval_groupBox.Controls.Add(this.radioButton1);
-            this.clickInterval_groupBox.Controls.Add(this.label7);
-            this.clickInterval_groupBox.Controls.Add(this.label6);
-            this.clickInterval_groupBox.Controls.Add(this.textBox5);
-            this.clickInterval_groupBox.Controls.Add(this.label5);
-            this.clickInterval_groupBox.Controls.Add(this.textBox4);
-            this.clickInterval_groupBox.Controls.Add(this.label4);
-            this.clickInterval_groupBox.Controls.Add(this.textBox3);
-            this.clickInterval_groupBox.Controls.Add(this.label3);
-            this.clickInterval_groupBox.Controls.Add(this.textBox2);
-            this.clickInterval_groupBox.Controls.Add(this.label2);
-            this.clickInterval_groupBox.Controls.Add(this.textBox1);
-            this.clickInterval_groupBox.Controls.Add(this.label1);
+            this.clickInterval_groupBox.Controls.Add(this.setTimeRand_radioButton);
+            this.clickInterval_groupBox.Controls.Add(this.setTimeDefault_radioButton);
+            this.clickInterval_groupBox.Controls.Add(this.end_label);
+            this.clickInterval_groupBox.Controls.Add(this.start_label);
+            this.clickInterval_groupBox.Controls.Add(this.end_textBox);
+            this.clickInterval_groupBox.Controls.Add(this.range_label);
+            this.clickInterval_groupBox.Controls.Add(this.start_textBox);
+            this.clickInterval_groupBox.Controls.Add(this.milliseconds_label);
+            this.clickInterval_groupBox.Controls.Add(this.milliseconds_textBox);
+            this.clickInterval_groupBox.Controls.Add(this.seconds_label);
+            this.clickInterval_groupBox.Controls.Add(this.seconds_textBox);
+            this.clickInterval_groupBox.Controls.Add(this.minutes_label);
+            this.clickInterval_groupBox.Controls.Add(this.minutes_textBox);
+            this.clickInterval_groupBox.Controls.Add(this.hours_label);
             this.clickInterval_groupBox.Controls.Add(this.hours_textBox);
             this.clickInterval_groupBox.Location = new System.Drawing.Point(12, 104);
             this.clickInterval_groupBox.Name = "clickInterval_groupBox";
@@ -71,14 +71,14 @@
             this.clickInterval_groupBox.TabStop = false;
             this.clickInterval_groupBox.Text = "Click Interval";
             // 
-            // label1
+            // hours_label
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 51);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.hours_label.AutoSize = true;
+            this.hours_label.Location = new System.Drawing.Point(68, 51);
+            this.hours_label.Name = "hours_label";
+            this.hours_label.Size = new System.Drawing.Size(33, 13);
+            this.hours_label.TabIndex = 2;
+            this.hours_label.Text = "hours";
             // 
             // hours_textBox
             // 
@@ -88,121 +88,121 @@
             this.hours_textBox.TabIndex = 1;
             this.hours_textBox.Text = "0";
             // 
-            // label2
+            // minutes_label
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(36, 77);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
+            this.minutes_label.AutoSize = true;
+            this.minutes_label.Location = new System.Drawing.Point(68, 77);
+            this.minutes_label.Name = "minutes_label";
+            this.minutes_label.Size = new System.Drawing.Size(43, 13);
+            this.minutes_label.TabIndex = 4;
+            this.minutes_label.Text = "minutes";
             // 
-            // textBox1
+            // minutes_textBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(178, 77);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(45, 20);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.Text = "0";
+            this.minutes_textBox.Location = new System.Drawing.Point(178, 77);
+            this.minutes_textBox.Name = "minutes_textBox";
+            this.minutes_textBox.Size = new System.Drawing.Size(45, 20);
+            this.minutes_textBox.TabIndex = 3;
+            this.minutes_textBox.Text = "0";
             // 
-            // label3
+            // seconds_label
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(36, 103);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "label3";
+            this.seconds_label.AutoSize = true;
+            this.seconds_label.Location = new System.Drawing.Point(68, 103);
+            this.seconds_label.Name = "seconds_label";
+            this.seconds_label.Size = new System.Drawing.Size(47, 13);
+            this.seconds_label.TabIndex = 6;
+            this.seconds_label.Text = "seconds";
             // 
-            // textBox2
+            // seconds_textBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(178, 103);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(45, 20);
-            this.textBox2.TabIndex = 5;
-            this.textBox2.Text = "0";
+            this.seconds_textBox.Location = new System.Drawing.Point(178, 103);
+            this.seconds_textBox.Name = "seconds_textBox";
+            this.seconds_textBox.Size = new System.Drawing.Size(45, 20);
+            this.seconds_textBox.TabIndex = 5;
+            this.seconds_textBox.Text = "0";
             // 
-            // label4
+            // milliseconds_label
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(36, 129);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "label4";
+            this.milliseconds_label.AutoSize = true;
+            this.milliseconds_label.Location = new System.Drawing.Point(68, 129);
+            this.milliseconds_label.Name = "milliseconds_label";
+            this.milliseconds_label.Size = new System.Drawing.Size(63, 13);
+            this.milliseconds_label.TabIndex = 8;
+            this.milliseconds_label.Text = "milliseconds";
             // 
-            // textBox3
+            // milliseconds_textBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(178, 129);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(45, 20);
-            this.textBox3.TabIndex = 7;
-            this.textBox3.Text = "0";
+            this.milliseconds_textBox.Location = new System.Drawing.Point(178, 129);
+            this.milliseconds_textBox.Name = "milliseconds_textBox";
+            this.milliseconds_textBox.Size = new System.Drawing.Size(45, 20);
+            this.milliseconds_textBox.TabIndex = 7;
+            this.milliseconds_textBox.Text = "0";
             // 
-            // label5
+            // range_label
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(36, 208);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(42, 13);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Range:";
+            this.range_label.AutoSize = true;
+            this.range_label.Location = new System.Drawing.Point(68, 211);
+            this.range_label.Name = "range_label";
+            this.range_label.Size = new System.Drawing.Size(107, 13);
+            this.range_label.TabIndex = 10;
+            this.range_label.Text = "Range(milliseconds) -";
             // 
-            // textBox4
+            // start_textBox
             // 
-            this.textBox4.Location = new System.Drawing.Point(178, 208);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(45, 20);
-            this.textBox4.TabIndex = 9;
-            this.textBox4.Text = "0";
+            this.start_textBox.Location = new System.Drawing.Point(178, 208);
+            this.start_textBox.Name = "start_textBox";
+            this.start_textBox.Size = new System.Drawing.Size(45, 20);
+            this.start_textBox.TabIndex = 9;
+            this.start_textBox.Text = "0";
             // 
-            // textBox5
+            // end_textBox
             // 
-            this.textBox5.Location = new System.Drawing.Point(242, 208);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(45, 20);
-            this.textBox5.TabIndex = 11;
-            this.textBox5.Text = "0";
+            this.end_textBox.Location = new System.Drawing.Point(242, 208);
+            this.end_textBox.Name = "end_textBox";
+            this.end_textBox.Size = new System.Drawing.Size(45, 20);
+            this.end_textBox.TabIndex = 11;
+            this.end_textBox.Text = "0";
             // 
-            // label6
+            // start_label
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(175, 192);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "label6";
+            this.start_label.AutoSize = true;
+            this.start_label.Location = new System.Drawing.Point(175, 192);
+            this.start_label.Name = "start_label";
+            this.start_label.Size = new System.Drawing.Size(32, 13);
+            this.start_label.TabIndex = 12;
+            this.start_label.Text = "Start:";
             // 
-            // label7
+            // end_label
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(239, 192);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "label7";
+            this.end_label.AutoSize = true;
+            this.end_label.Location = new System.Drawing.Point(239, 192);
+            this.end_label.Name = "end_label";
+            this.end_label.Size = new System.Drawing.Size(29, 13);
+            this.end_label.TabIndex = 13;
+            this.end_label.Text = "End:";
             // 
-            // radioButton1
+            // setTimeDefault_radioButton
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(39, 31);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(110, 17);
-            this.radioButton1.TabIndex = 14;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Set Time(Default):";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.setTimeDefault_radioButton.AutoSize = true;
+            this.setTimeDefault_radioButton.Location = new System.Drawing.Point(39, 31);
+            this.setTimeDefault_radioButton.Name = "setTimeDefault_radioButton";
+            this.setTimeDefault_radioButton.Size = new System.Drawing.Size(110, 17);
+            this.setTimeDefault_radioButton.TabIndex = 14;
+            this.setTimeDefault_radioButton.TabStop = true;
+            this.setTimeDefault_radioButton.Text = "Set Time(Default):";
+            this.setTimeDefault_radioButton.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // setTimeRand_radioButton
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(39, 188);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(116, 17);
-            this.radioButton2.TabIndex = 15;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Set Time(Random):";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.setTimeRand_radioButton.AutoSize = true;
+            this.setTimeRand_radioButton.Location = new System.Drawing.Point(39, 188);
+            this.setTimeRand_radioButton.Name = "setTimeRand_radioButton";
+            this.setTimeRand_radioButton.Size = new System.Drawing.Size(116, 17);
+            this.setTimeRand_radioButton.TabIndex = 15;
+            this.setTimeRand_radioButton.TabStop = true;
+            this.setTimeRand_radioButton.Text = "Set Time(Random):";
+            this.setTimeRand_radioButton.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
@@ -224,20 +224,20 @@
 
         private System.Windows.Forms.GroupBox clickInterval_groupBox;
         private System.Windows.Forms.TextBox hours_textBox;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label hours_label;
+        private System.Windows.Forms.Label range_label;
+        private System.Windows.Forms.TextBox start_textBox;
+        private System.Windows.Forms.Label milliseconds_label;
+        private System.Windows.Forms.TextBox milliseconds_textBox;
+        private System.Windows.Forms.Label seconds_label;
+        private System.Windows.Forms.TextBox seconds_textBox;
+        private System.Windows.Forms.Label minutes_label;
+        private System.Windows.Forms.TextBox minutes_textBox;
+        private System.Windows.Forms.Label end_label;
+        private System.Windows.Forms.Label start_label;
+        private System.Windows.Forms.TextBox end_textBox;
+        private System.Windows.Forms.RadioButton setTimeRand_radioButton;
+        private System.Windows.Forms.RadioButton setTimeDefault_radioButton;
     }
 }
 
