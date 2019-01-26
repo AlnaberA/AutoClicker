353c18f3876f70d41e2b1c726a5fb4b23d6e1dab
Alaa Al Naber <alnaberalaa@gmail.com>
2019-01-26 12:21:30 -0500

    https://www.youtube.com/watch?v=duBvDBEZlbE, stop until f9 is pressed. using background worker to handle run button. TODO: Clean up code.

--- AutoClicker/MainForm.cs
+++ AutoClicker/MainForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,11 +13,42 @@ namespace AutoClicker
 {
     public partial class MainForm : Form
     {
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+
+        private bool flag;
+
         public MainForm()
         {
             InitializeComponent();
             //Sets default select box items
             mouseBtn_comboBox.SelectedItem = "Single Left Click";
+
+            // 3. Register HotKey
+
+            // Set an unique id to your Hotkey, it will be used to
+            // identify which hotkey was pressed in your code to execute something
+            int UniqueHotkeyId = 1;
+            // Set the Hotkey triggerer the F9 key 
+            // Expected an integer value for F9: 0x78, but you can convert the Keys.KEY to its int value
+            // See: https://msdn.microsoft.com/en-us/library/windows/desktop/dd375731(v=vs.85).aspx
+            int HotKeyCode = (int)Keys.F9;
+            // Register the "F9" hotkey
+            Boolean F9Registered = RegisterHotKey(
+                this.Handle, UniqueHotkeyId, 0x0000, HotKeyCode
+            );
+
+            // 4. Verify if the hotkey was succesfully registered, if not, show message in the console
+            if (F9Registered)
+            {
+                Console.WriteLine("Global Hotkey F9 was succesfully registered");
+            }
+            else
+            {
+                Console.WriteLine("Global Hotkey F9 couldn't be registered !");
+            }
+
+            this.flag = false;
         }
 
         private void setTimeDefault_radioButton_CheckedChanged(object sender, EventArgs e)
@@ -85,45 +117,70 @@ namespace AutoClicker
      
             mouseClicksInfo_dataGridView.Rows.Add(row);
         }
-
+        BackgroundWorker BackgroundWorker;
         private void run_button_Click(object sender, EventArgs e)
         {
+            BackgroundWorker = new BackgroundWorker();
+            BackgroundWorker.DoWork += (obj, ea) => TaskAsync(1);
+            BackgroundWorker.RunWorkerAsync();
+        }
 
-
-            // Set the Current cursor, move the cursor's Position
-            foreach (DataGridViewRow row in mouseClicksInfo_dataGridView.Rows)
+        private async void TaskAsync(int times)
+        {
+            this.flag = true;
+            while (this.flag)
             {
-                //Ignore last row
-                if (row.IsNewRow)
+                // Set the Current cursor, move the cursor's Position
+                foreach (DataGridViewRow row in mouseClicksInfo_dataGridView.Rows)
                 {
-                    continue;
-                }
-
-                if (setTimeDefault_radioButton.Enabled == true)
-                {
-                    if (Convert.ToInt16(milliseconds_textBox.Text) != 0)
+                    //Ignore last row
+                    if (row.IsNewRow)
                     {
-                        System.Threading.Thread.Sleep(Convert.ToInt16(milliseconds_textBox.Text));
+                        continue;
+                    }
+
+                    if (setTimeDefault_radioButton.Enabled == true)
+                    {
+                        if (Convert.ToInt16(milliseconds_textBox.Text) != 0)
+                        {
+                            System.Threading.Thread.Sleep(Convert.ToInt16(milliseconds_textBox.Text));
+                        }
+                    }
+                    else if (setTimeRand_radioButton.Enabled == true)
+                    {
+                        System.Threading.Thread.Sleep(RandomNumber(Convert.ToInt16(start_textBox.Text), Convert.ToInt16(end_textBox.Text)));
+                    }
+
+                    Mouse.SetCursorPosition(Convert.ToInt16(row.Cells[2].Value), Convert.ToInt16(row.Cells[3].Value));
+
+                    if (row.Cells[1].Value.ToString() == "Single Left Click")
+                    {
+                        Mouse.MouseEvent(Mouse.MouseEventFlags.LeftDown);
+                        Mouse.MouseEvent(Mouse.MouseEventFlags.LeftUp);
+                    }
+                    else if (row.Cells[1].Value.ToString() == "Single Right Click")
+                    {
+                        Mouse.MouseEvent(Mouse.MouseEventFlags.RightDown);
+                        Mouse.MouseEvent(Mouse.MouseEventFlags.RightUp);
                     }
                 }
-                else if (setTimeRand_radioButton.Enabled == true)
-                {
-                    System.Threading.Thread.Sleep(RandomNumber(Convert.ToInt16(start_textBox.Text), Convert.ToInt16(end_textBox.Text)));
-                }
+            }
+        }
+        protected override void WndProc(ref Message m)
+        {
+            // 5. Catch when a HotKey is pressed !
+            if (m.Msg == 0x0312)
+            {
+                int id = m.WParam.ToInt32();
+                // MessageBox.Show(string.Format("Hotkey #{0} pressed", id));
 
-                Mouse.SetCursorPosition(Convert.ToInt16(row.Cells[2].Value), Convert.ToInt16(row.Cells[3].Value));
-
-                if (row.Cells[1].Value.ToString() == "Single Left Click")
+                if (id == 1)
                 {
-                    Mouse.MouseEvent(Mouse.MouseEventFlags.LeftDown);
-                    Mouse.MouseEvent(Mouse.MouseEventFlags.LeftUp);
-                }
-                else if (row.Cells[1].Value.ToString() == "Single Right Click")
-                {
-                    Mouse.MouseEvent(Mouse.MouseEventFlags.RightDown);
-                    Mouse.MouseEvent(Mouse.MouseEventFlags.RightUp);
+                    //MessageBox.Show("F9 Was pressed !");
+                    this.flag = false;
                 }
             }
+            base.WndProc(ref m);
         }
 
         private int RandomNumber(int startNumber, int endNumber)
